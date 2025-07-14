7b06d74b5a0b44fed84c1258f0cc25a8293e34bb
Mark-Canubas <Canubasmarkroland@gmail.com>
2025-07-14 08:58:57 +0800

    Add project files.

---
+++ GetClientIPDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YourAppNamespace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string clientIp = GetClientIp();
+
+            stopwatch.Stop();
+            TimeSpan elapsed = stopwatch.Elapsed;
+
+            ViewBag.Ip = clientIp;
+            ViewBag.Elapsed = $"{elapsed.TotalMilliseconds} ms";
+
+            return View();
+        }
+
+        private string GetClientIp()
+        {
+            var request = System.Web.HttpContext.Current?.Request;
+            string ip = request?.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (!string.IsNullOrEmpty(ip))
+            {
+                return ip.Split(',')[0];
+            }
+
+            return request?.UserHostAddress ?? "IP Not Available";
+        }
+    }
+}
---
+++ GetClientIPDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GetClientIPDemo.MvcApplication" Language="C#" %>
